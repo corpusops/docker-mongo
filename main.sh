34218ff90c0ed3390359fc68e9e5452ae94320cc
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2025-09-01 20:00:39 +0300

    Refresh & squash

--- main.sh
+++ main.sh
@@ -260,7 +260,7 @@ SKIPPED_TAGS="$SKIP_TF|$SKIP_MINOR_OS|$SKIP_NODE|$SKIP_DOCKER|$SKIP_MINIO|$SKIP_
 CURRENT_TS=$(date +%s)
 IMAGES_SKIP_NS="((mailhog|postgis|pgrouting(-bare)?|^library|dejavu|(minio/(minio|mc))))"
 
-MONGO_SKIPPED_TAGS="mongo:(rc|2|(3|4|5|6|7)\.[0-9]+|.*(noble|xenial|bionic|stretch|jessie|focal|jammy|windows|nano|-rc|[0-9]+\.[0-9]+\.[0-9]+).*)"
+MONGO_SKIPPED_TAGS="mongo:(rc|2|3|(4|5|6|7)\.[0-9]+|.*(unstable|noble|xenial|bionic|stretch|jessie|focal|jammy|windows|nano|-rc|[0-9]+\.[0-9]+\.[0-9]+).*)"
 SKIPPED_TAGS="$MONGO_SKIPPED_TAGS"
 PROTECTED_VERSIONS=""
 
@@ -290,8 +290,7 @@ NODE_TOP="$(echo $(find_top_node))"
 MAILU_VERSiON=1.7
 
 BATCHED_IMAGES="\
-library/mongo/latest\
- library/mongo/unstable::30
+library/mongo/latest::30
 library/mongo/3\
  library/mongo/4\
  library/mongo/5\
